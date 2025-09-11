425faf39385250d58919a807219c282e165e4fe9
FounDy <vkadgcundgd@gmail.com>
2025-09-11 20:55:53 +0200

    yep

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -34,9 +34,8 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.string().pattern(/^\+?\d{10,15}$/),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('personal', 'work', 'other'),
 });
